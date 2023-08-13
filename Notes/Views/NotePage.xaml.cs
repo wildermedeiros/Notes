2606e5332099d88d8ee9b91f07f41b566cc306ab
Wilder Medeiros <wildersmedeiros@gmail.com>
2023-08-12 22:47:19 -0300

    Initial commit

---
+++ Notes/Views/NotePage.xaml.cs
@@ -0,0 +1,43 @@
+namespace Notes.Views;
+
+public partial class NotePage : ContentPage
+{
+    string _fileName = Path.Combine(FileSystem.AppDataDirectory, "notes.txt");
+
+    public NotePage()
+    {
+        InitializeComponent();
+
+        if (File.Exists(_fileName))
+            TextEditor.Text = File.ReadAllText(_fileName);
+    }
+
+    private void SaveButton_Clicked(object sender, EventArgs e)
+    {
+        // Save the file.
+        File.WriteAllText(_fileName, TextEditor.Text);
+    }
+
+    private void DeleteButton_Clicked(object sender, EventArgs e)
+    {
+        // Delete the file.
+        if (File.Exists(_fileName))
+            File.Delete(_fileName);
+
+        TextEditor.Text = string.Empty;
+    }
+
+    private void LoadNote(string fileName)
+    {
+        Models.Note noteModel = new Models.Note();
+        noteModel.Filename = fileName;
+
+        if (File.Exists(fileName))
+        {
+            noteModel.Date = File.GetCreationTime(fileName);
+            noteModel.Text = File.ReadAllText(fileName);
+        }
+
+        BindingContext = noteModel;
+    }
+}
